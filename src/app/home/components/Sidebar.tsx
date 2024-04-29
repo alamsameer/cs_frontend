edc14fdcdadee65aad0f780e6ddd2260f7ffcf96
Sameer Alam <alamsameer738@gmail.com>
2024-04-29 09:04:00 +0530

    updated name

--- src/app/home/components/Sidebar.tsx
+++ src/app/home/components/Sidebar.tsx
@@ -11,7 +11,7 @@ export default function Sidebar({toggleNav,isNavOpen}: SideBarProps) {
 
   return (
     <div className={`z-20 bg-primary shrink-0 transition-all ${isNavOpen?"":"-translate-x-full "} w-48 h-full space-y-1 fixed top-0 bottom-0 left-0 sm:relative`}>
-      <div><p className=" text-4xl text-center my-3 font-bold">logo</p></div>
+      <div><p className=" text-4xl text-center my-3 font-bold text-white ">Mandi</p></div>
         <button  onClick={()=>{toggleNav()}} className="w-full p-2 text-white flex justify-center sm:hidden"><RxCrossCircled className="w-[1.8rem] h-[1.8rem]" /></button>
         <button className="text-foreground w-full px-2 py-1 lg:px-3 hover:bg-secondary my-2 rounded-md"><Link to={'/'}>Dashboard</Link></button>
         <button className="text-foreground w-full px-2 py-1 lg:px-3 hover:bg-secondary my-2 rounded-md"><Link to={'/inventory'}>Inventory</Link></button>
